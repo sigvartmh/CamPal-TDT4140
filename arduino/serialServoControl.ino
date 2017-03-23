39cd6d398a2ac3120b8242ae0bb46bb71aa8466b
Morten Rostad <mrostad@gmail.com>
2017-03-23 16:38:14 +0100

    Added arduino server control code

---
+++ arduino/serialServoControl.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+
+#define SERVO_X_PIN 5
+
+Servo servoX;
+
+int pos = 0;
+
+void setup() {
+  Serial.begin(9600);
+  servoX.attach(SERVO_X_PIN);
+  servoX.write(90); //center camera
+}
+
+void loop() {
+  //Serial.print("Pos: ");
+  //Serial.println(pos);
+  if (pos > 170) {
+    pos = 170;
+  }
+  if (pos < 12) {
+    pos = 12;
+  }
+  servoX.write(pos);
+}
+
+void serialEvent() {
+  pos = Serial.parseInt();
+}
+
