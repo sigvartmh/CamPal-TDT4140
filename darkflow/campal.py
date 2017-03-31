62055790d7a110d57ef5fa452f7c3bea7779d7b9
sigvartmh <sigvart.m@gmail.com>
2017-04-01 01:11:28 +0200

    Add multithreading of neural network and video capture

--- darkflow/campal.py
+++ darkflow/campal.py
@@ -2,36 +2,81 @@ from net.build import TFNet
 from tensorflow import flags
 import cv2
 import unittest
+import cProfile, pstats, io
+import threading, queue
 
-class objectTracker:
+trackerQueue = queue.Queue(1)
+resultQueue = queue.Queue(1)
+tfFrame = queue.Queue()
+#fourcc = cv2.VideoWriter_fourcc('P', 'I', 'M', '1')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+#out = cv2.VideoWriter('test.avi', fourcc, 20.0, (int(cap.get(WIDTH)), int(cap.get(HEIGHT))))
+ratio = 4
+#options = { "model": "cfg/tiny-yolo-voc.cfg", "load": "bin/tiny-yolo-voc.weights", "threshold": 0.5, "gpu": 1.0}
+options = { "model": "cfg/yolo-voc.cfg", "load": "bin/yolo-voc.weights", "threshold": 0.5, "gpu": 1.0}
+
+class ObjectTracker(threading.Thread):
+    def run(self):
+        self.setup()
+        while True:
+            frame = trackerQueue.get()
+            tfframe = cv2.resize(frame, (0,0), fx=1/ratio, fy=1/ratio)
+            print(tfframe.shape)
+            result = self.tfnet.return_predict(tfframe)
+            resultQueue.put(result)
+            tfFrame.put(tfframe)
+    def setup(self):
+        self.tfnet = TFNet(options)
+
+class VideoCapture():
+    def run(self):
+        self.setup()
+        while True:
+            ret, frame = self.cap.read()
+            if(trackerQueue.empty()):
+                trackerQueue.put_nowait(frame)
+            elif(trackerQueue.full()):
+                trackerQueue.get_nowait()
+            if(resultQueue.full()):
+                self.results = resultQueue.get()
+            if(self.results):
+                for item in self.results:
+                    try:
+                        (pt1, pt2) = self.findPerson(item)
+                        #print("{0} and {1}".format(pt1,pt2))
+                        cv2.rectangle(frame, pt1, pt2, (255,0,0), 2)
+                        cv2.rectangle(frame, (pt1[0]-1,pt1[1]-1), (pt1[0]+150,pt1[1]-30), (255,0,0), -1)
+                        cv2.putText(frame, item['label'] , (pt1[0]+2,pt1[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),2)
+                        pos = int((((pt1[0]+pt2[0])/2)/MAX_WIDTH)*180)
+                    except:
+                        pass
+            cv2.imshow('frame',frame)
+            if not tfFrame.empty():
+                tfframe = tfFrame.get()
+                cv2.imshow('tfFrame',tfframe)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+        '''
+                print("NoneType")
+        '''
+    def setup(self):
+        WIDTH=3
+        HEIGHT=4
+        self.results = None
+        self.cap = cv2.VideoCapture(0)
+        self.MAX_WIDTH=self.cap.get(WIDTH)
+        self.MAX_HEIGHT=self.cap.get(HEIGHT)
+
     def findPerson(self, detectedObject):
         if(detectedObject['label'] == 'person'):
-            pt1 = (detectedObject['topleft']['x'],detectedObject['topleft']['y'])
-            pt2 = (detectedObject['bottomright']['x'],detectedObject['bottomright']['y'])
+            pt1 = (detectedObject['topleft']['x']*ratio,detectedObject['topleft']['y']*ratio)
+            pt2 = (detectedObject['bottomright']['x']*ratio,detectedObject['bottomright']['y']*ratio)
             return (pt1, pt2)
 
-tracker = objectTracker()
-
-options = { "model": "cfg/yolo-voc.cfg", "load": "bin/yolo-voc.weights", "threshold": 0.1}
-tfnet = TFNet(options)
-
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    result = tfnet.return_predict(frame)
-    for item in result:
-        print(item)
-        try:
-            (pt1, pt2) = tracker.findPerson(item)
-            cv2.rectangle(frame, pt1, pt2, (255,0,0), 2)
-            cv2.rectangle(frame, (pt1[0]-1,pt1[1]-1), (pt1[0]+150,pt1[1]-30), (255,0,0), -1)
-            cv2.putText(frame, item['label'] , (pt1[0]+2,pt1[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),2)
-        except:
-            print("NoneType")
-    cv2.imshow('frame',frame)
-    #print(result)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-cap.release()
-cv2.destroyAllWindows()
+ObjectTracker().start()
+VideoCapture().run()
 
