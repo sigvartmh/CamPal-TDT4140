1b1782be1b4889340f7cf654032f2dcf017d3fb6
sigvartmh <sigvart.m@gmail.com>
2017-04-01 23:02:37 +0200

    add gcal api

--- darkflow/campal.py
+++ darkflow/campal.py
@@ -1,9 +1,21 @@
 from net.build import TFNet
 from tensorflow import flags
+
 import cv2
-import unittest
 import cProfile, pstats, io
 import threading, queue
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
+
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'gcal.json'
+APPLICATION_NAME = 'CamPals scheduler'
 
 trackerQueue = queue.Queue(1)
 resultQueue = queue.Queue(1)
@@ -15,6 +27,36 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 ratio = 4
 #options = { "model": "cfg/tiny-yolo-voc.cfg", "load": "bin/tiny-yolo-voc.weights", "threshold": 0.5, "gpu": 1.0}
 options = { "model": "cfg/yolo-voc.cfg", "load": "bin/yolo-voc.weights", "threshold": 0.5, "gpu": 1.0}
+
+class GCalender(object):
+    def __init__(self):
+        get_credentials()
+    def get_credentials(self):
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(crendetial_dir)
+        credential_path = os.path.join(credential_dir, 'gcal_credentials.json')
+        store = Storage(credential_path)
+        credentials = store.get()
+
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            if flags:
+                credentials = tools.run_flow(flow, store, flags)
+        self.credentials=credentials
+
+    def get_gcal_service(self):
+        http = self.credentials.authorize(httplib2.Http())
+        service = discovery.build('calendar', 'v3', http=http)
+        return service
+
+    def get_events(self):
+        service = self.get_gcal_service()
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        eventResult = service.events.list(calendarId='primary', timeMin=now, maxResults=12, singleEvents=True, orderBy='startTime').execute()
+        return eventsResult.get('items', [])
 
 class ObjectTracker(threading.Thread):
     def run(self):
@@ -77,6 +119,8 @@ class VideoCapture():
             pt2 = (detectedObject['bottomright']['x']*ratio,detectedObject['bottomright']['y']*ratio)
             return (pt1, pt2)
 
-ObjectTracker().start()
-VideoCapture().run()
+if __name__ == '__main__':
+
+    ObjectTracker().start()
+    VideoCapture().run()
 
